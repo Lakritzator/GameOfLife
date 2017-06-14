7f84339a3fe6b090f16ee3e21401981abec75dcf
Krom, Robertus <Robertus.Krom@ing-diba.de>
2017-06-14 12:47:27 +0200

    Moving to Dapplo.

---
+++ GameOfLife/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using Dapplo.CaliburnMicro.Dapp;
+using Dapplo.Log;
+using Dapplo.Log.Loggers;
+
+namespace GameOfLife
+{
+    static class Startup
+    {
+        /// <summary>
+        /// Start the application
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Start();
+        }
+
+        public static void Start()
+        {
+#if DEBUG
+            LogSettings.RegisterDefaultLogger<DebugLogger>(LogLevels.Verbose);
+#else
+            // Register a default logger which will forward all log statements to the next default logger, this way nothing is missed.
+            LogSettings.RegisterDefaultLogger<ForwardingLogger>(LogLevels.Verbose);
+#endif
+
+            var application = new Dapplication("GetMyLogs", "a5f6aa70-b899-4691-a9ac-94f86b7eb555")
+            {
+                ShutdownMode = ShutdownMode.OnMainWindowClose
+            };
+            application.Bootstrapper.FindAndLoadAssemblies("Dapplo.CaliburnMicro*");
+
+            application.Run();
+        }
+    }
+}
